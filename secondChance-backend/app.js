9628c3269f07febca90975f2c561d7bf8b3dcb6b
Sidhartha Sankar Swain <sidharthasankarswain425@gmail.com>
2025-07-17 10:43:35 -0400

    Create API for secondChanceItems Service

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 //{{insert code here}}
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
@@ -36,14 +36,14 @@ app.use(express.json());
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
-app.use(pinoHttp({ logger }));
+//app.use(pinoHttp({ logger }));
 
 // Use Routes
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
 //{{insert code here}}
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
